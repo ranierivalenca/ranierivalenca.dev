cbe5b2a1d35f2d8e46e923b5f1cbce5f68ed261a
Ranieri Valença <ranierivalenca@gmail.com>
2022-03-31 10:06:12 -0300

    adding landing component

---
+++ src/lib/Landing.svelte
@@ -0,0 +1,23 @@
+<div id="landing">
+  <img alt="ranieri" src="photo.jpg">
+  <h1>Ranieri Valença de Carvalho</h1>
+  <h2>Full Stack Web Developer and Teacher</h2>
+</div>
+
+<style type="text/scss">
+  #landing {
+    @apply flex flex-col justify-center p-6 font-mono;
+    img {
+      @apply block m-auto mb-2;
+      @apply w-96 rounded-full border-2 shadow-lg border-white;
+    }
+    h1, h2 {
+      @apply block m-auto;
+      @apply text-3xl;
+    }
+    h2 {
+      @apply text-2xl;
+    }
+    @apply border-b ml-4 mr-20;
+  }
+</style>
